dea3cd6891e8b7d2754e1992f3b811a0ffce6cff
GravySlayer <tylersmoore3@gmail.com>
2013-04-22 14:23:03 -0500

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,23 @@
+public class Enemy extends Characters {
+  
+	private static int CalcHealth(int floor)
+  {
+
+		if (floor == 1)
+			return 75;
+		if (floor == 2)
+			return (int)(Math.random()*50 + 100);
+		if (floor == 3)
+			return (int)(Math.random()*100 + 125);
+		if (floor == 4)
+			return (int)(Math.random()*150 + 150);
+		if (floor == 5) 
+			return (int)(math.random()*200 + 200);
+		
+	}
+  
+  private static int CalcStrength(int floor)
+  {
+    return (int)(Math.random()*20+25*floor);
+  }
+}
