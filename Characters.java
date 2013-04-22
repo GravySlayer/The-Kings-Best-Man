7359e8d99a7042ecce7788052f9bbe48d47bc1d3
Sean Petrie <volitionadict@hotmail.com>
2013-04-22 04:31:51 -0500

    Update Characters.java
    Altered the miss assignment for simplicity.

--- Characters.java
+++ Characters.java
@@ -2,13 +2,8 @@ public class Playable extends Characters {
 
   public static int attack(int strength)
 	{
-		boolean miss;
 		int chance = (int)(100*Math.random());
-		if (chance > 90)
-			miss = true;
-		else
-			miss = false;
-		return (miss) ? 0:(int)(.5*Math.random()*strength+strength);
-		
+		boolean miss = (chance > 90) ? true:false;
+		return (miss) ? 0:(int)(0.5 * Math.random() * strength + strength);	
 	};
 }
