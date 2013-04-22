4057c4a5b2aea789c7063a37243e91b78406a559
GravySlayer <tylersmoore3@gmail.com>
2013-04-22 18:34:10 -0500

    Update Characters.java

--- Characters.java
+++ Characters.java
@@ -1,8 +1,12 @@
-public class Playable extends Characters {
+public class Characters {
+
+protected int x;
+protected int y;
 
 public int attackDamage(int strength)
 {
 	int chance = (int)(100*Math.random());
 	boolean miss = (chance > 90) ? true:false;
-	return (miss) ? 0:(int)(0.5 * Math.random() * strength + strength);	
+	return (miss) ? 0:(int)(0.5 * Math.random() * strength + strength);		
 };
+}
