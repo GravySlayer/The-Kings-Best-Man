620b1cd0405475ce6d48b8baa899a043259c838d
Sean Petrie <volitionadict@hotmail.com>
2013-04-23 22:27:43 -0500

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -2,33 +2,31 @@ package csc122.projects.kings;
 
 public class Enemy extends Characters {
 
-	Enemy(int floor, int x, int y)
-	{
+	Enemy(int floor, int x, int y, String name) {
 		maxVitality = CalcHealth(floor);
 		currentVitality = maxVitality;
 		strength = CalcStrength(floor);
 		this.x = x;
 		this.y = y;
+		this.name = name;
 	}
-  
-	private static int CalcHealth(int floor)
- 	{
+
+	private static int CalcHealth(int floor) {
 
 		if (floor == 1)
-			return (int)75;
+			return (int) 75;
 		if (floor == 2)
-			return (int)(Math.random()*50 + 100);
+			return (int) (Math.random() * 50 + 100);
 		if (floor == 3)
-			return (int)(Math.random()*100 + 125);
+			return (int) (Math.random() * 100 + 125);
 		if (floor == 4)
-			return (int)(Math.random()*150 + 150);
+			return (int) (Math.random() * 150 + 150);
 		else
-			return (int)(Math.random()*200 + 200);
+			return (int) (Math.random() * 200 + 200);
 
 	}
-  
- 	private static int CalcStrength(int floor)
- 	{
-  		return (int)(Math.random()*20+25*floor);
- 	}
+
+	private static int CalcStrength(int floor) {
+		return (int) (Math.random() * 20 + 25 * floor);
+	}
 }
