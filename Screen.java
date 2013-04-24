5b4d26009e2b761ae37ae8e0a474088c8143b485
amozew <amwhitaker95@gmail.com>
2013-04-24 15:25:59 -0500

    Create Screen.java

---
+++ Screen.java
@@ -0,0 +1,51 @@
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class Screen
+{
+  //allows access to graphics card
+	private GraphicsDevice videocard;
+
+	public Screen()
+	{
+		//graphics environment - collection of all graphics device objects
+		GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		videocard = env.getDefaultScreenDevice();
+	}
+
+	public void setFullScreen(DisplayMode dm, JFrame window)
+	{
+		//gets rid of everything aside from image
+		window.setUndecorated(true);	
+		//doesn't let you resize
+		window.setResizable(false);
+		//sets to full screen
+		videocard.setFullScreenWindow(window);
+
+		if(dm != null && videocard.isDisplayChangeSupported())
+		{
+			try 
+			{
+				videocard.setDisplayMode(dm);
+			}
+			catch(Exception e){}
+		}
+	}
+
+	public Window getFullScreenWindow()
+	{
+		return videocard.getFullScreenWindow();
+	}
+
+	//method so you're not stuck in King's Best Man forever
+	public void restoreScreen()
+	{
+		Window w = videocard.getFullScreenWindow();
+		if (w != null)
+		{
+			w.dispose();
+		}
+		videocard.setFullScreenWindow(null);
+	}
+
+}
