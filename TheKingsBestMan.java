7dc181f9c5fd7617201593d31601563f7f385651
Sean Petrie <volitionadict@hotmail.com>
2013-04-23 23:11:28 -0500

    Update TheKingsBestMan.java

--- TheKingsBestMan.java
+++ TheKingsBestMan.java
@@ -3,6 +3,7 @@ package csc122.projects.kings;
 public class TheKingsBestMan {
 
 	public static void main(String[] args) {
+		// Files used in current version found here: https://www.dropbox.com/sh/zmmtvvturfa5bnl/7aGfqtkl5t
 		Floor one = new Floor("1");
 		one.printPathData();
 		one.printEnemyList();
