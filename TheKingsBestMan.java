34b3b02f48fc889eb5dab8761c64d981386ecc86
Sean Petrie <volitionadict@hotmail.com>
2013-04-23 23:09:20 -0500

    Update TheKingsBestMan.java

--- TheKingsBestMan.java
+++ TheKingsBestMan.java
@@ -1,36 +1,10 @@
 package csc122.projects.kings;
 
-import java.util.ArrayList;
+public class TheKingsBestMan {
 
-public class TheKingsBestMan { 
-	static ArrayList<Enemy> enemyList;
-	
 	public static void main(String[] args) {
-		/*floor one = new floor("1");
-		monsterSpawn(one);
+		Floor one = new Floor("1");
 		one.printPathData();
-		System.out.println('\n');
-		for (Enemy enemy : enemyList) {
-			System.out.println(enemy.name + "  (" + enemy.x + ',' + enemy.y + ')');
-		}*/		
-	}
-
-	public static void monsterSpawn(floor level) {
-		enemyList = new ArrayList<Enemy>();
-		int nameCounter = 0;
-		for (int row = 0; row < level.getdimY(); row++) {
-			for (int col = 0; col < level.getdimX(); col++) {
-				switch (level.getPathTile(row, col)) {
-				case SPAWN:
-					enemyList.add(new Enemy(level.getLevel(), row, col, "actor_" + nameCounter));
-					nameCounter++;
-					break;
-				case SPAWN_RANDOM:
-					break;
-				default:
-					break;
-				}
-			}
+		one.printEnemyList();
 		}
 	}
-}
