6c03c61443f4fba4056f16cd5ecd3594dabcfb26
Sean Petrie <volitionadict@hotmail.com>
2013-04-23 22:32:58 -0500

    Update and rename renderTile.java to RenderTile.java

--- RenderTile.java
+++ RenderTile.java
@@ -1,25 +1,29 @@
 package csc122.projects.kings;
 
-public enum renderTile {
-  STONE(-1, 'S', ""); // (255,255,255)
+public enum RenderTile {
+	STONE(-1, 'S', ""), // (255,255,255)
+	UNKNOWN(0, '?', ""); //(-1, -1, -1)
 
 	
 	protected final int value;
 	protected final char shorthand;
 	protected final String filePath;
 
-	 renderTile(int value, char shorthand, String filePath){
+	 RenderTile(int value, char shorthand, String filePath){
 		this.value = value;
 		this.shorthand = shorthand;
 		this.filePath = filePath;		
 	}
 	 // Converts the color int values into enumerated tiles
-	static renderTile toRenderTile(int in){
-		renderTile result;
+	static RenderTile toRenderTile(int in){
+		RenderTile result;
 		switch(in){
+		case -1:
+			result = STONE;
+			break;
 		default:
 			System.out.println("Unknown value: " + in);
-			result = STONE;
+			result = UNKNOWN;
 			break;
 		}
 		return result;
