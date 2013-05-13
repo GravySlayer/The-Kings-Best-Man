30f8418cade2b1f8daa25f4ddff5b34152f762d0
GravySlayer <tylersmoore3@gmail.com>
2013-05-13 15:05:35 -0400

    Update Animation.java

--- Animation.java
+++ Animation.java
@@ -1,3 +1,4 @@
+package csc122.projects.kings;
 import java.awt.Image;
 import java.util.ArrayList;
 
@@ -7,7 +8,7 @@ public class Animation
 	private int sceneIndex;
 	private long movieTime;
 	private long totalTime;
-	
+
 	//constructor
 	private Animation()
 	{
@@ -15,21 +16,21 @@ public class Animation
 		totalTime = 0;
 		start();
 	}
-	
+
 	//add scene to array and set time for each
 	public synchronized void addScene(Image i, long t)
 	{
 		totalTime += t;
 		scenes.add(new OneScene(i, totalTime));
 	}
-	
+
 	//start animation from beginning
 	public synchronized void start()
 	{
 		movieTime = 0;
 		sceneIndex = 0;
 	}
-	
+
 	//change scenes
 	public synchronized void update(long timePassed)
 	{
@@ -41,14 +42,14 @@ public class Animation
 				movieTime = 0;
 				sceneIndex = 0;
 			}
-			
+
 			while(movieTime > getScene(sceneIndex).endTime)
 			{
 				sceneIndex++;
 			}
 		}
 	}
-	
+
 	//get animation current image
 	public synchronized Image getImage()
 	{
@@ -61,24 +62,24 @@ public class Animation
 			return getScene(sceneIndex).pic;
 		}
 	}
-	
+
 	//get scene
 	private OneScene getScene(int x)
 	{
 		return (OneScene)scenes.get(x);
 	}
-	
+
 	//private inner class - if you create image, it's object of this class
 	private class OneScene
 	{
 		Image pic;
 		long endTime;
-		
+
 		public OneScene(Image pic, long endTime)
 		{
 			this.pic = pic;
 			this.endTime = endTime;
 		}
 	}
-	
+
 }
